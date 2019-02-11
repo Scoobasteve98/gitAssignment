059e5a8a2f379f9eecf82576f63c3d452a0853f2
scoob <scangel@oakland.edi>
2019-02-11 16:49:01 -0500

    Implemented both methods main and ninetyNineBottlesOfBeer in the class BeerSong

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class BeerSong {
+    public static void main(String[] args) {
+        for(int i = 99; i != 0; i--) {
+            ninetyNineBottlesOfBeer(i);
+        }
+    }
+
+    public static void ninetyNineBottlesOfBeer(int bottles) {
+        System.out.println(bottles + " bottles of beer on the wall, "+ bottles +" bottles of beer,");
+        System.out.println("take one down, pass it around, " +(bottles - 1)+ " bottles of beer on the wall");
+    }
 }
